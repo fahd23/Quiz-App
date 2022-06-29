f38751ffb9c98b54e400b687649fcd6d11714724
fahd23 <fahdpno@gmail.com>
2022-06-29 16:06:03 +0530

    FEAT: home-page

--- src/components/Quiz-Categories/Categories.js
+++ src/components/Quiz-Categories/Categories.js
@@ -1,30 +1,35 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import "./categories.css";
+
 const Categories = () => {
-  const navigate = useNavigate();
-  const clickHandler = () => {
-    navigate("/rules");
-  };
   return (
-    <div>
-      <section className="quiz-type" onClick={() => clickHandler("HTML")}>
-        <div className="quiz-type-one">
-          <img src="images/general.jpg" alt="general quiz" className="image" />
+    <div className="container">
+      <h1 className="title">Select the quiz category</h1>
+      <div className="category-container">
+        <div className="quiz-card">
+          <img
+            src="Assets/html-quiz.png"
+            alt="HTML quiz"
+            className="card-image"
+          />
           <div className="image-text">
             <h3>HTML</h3>
-            <p className="details">Test Your HTML Understanding</p>
+            <p className="details">Test Your HTML Understanding here</p>
           </div>
         </div>
-      </section>
-      <section className="quiz-type" onClick={() => clickHandler("CSS")}>
-        <div className="quiz-type-two">
-          <img src="images/harry.jpg" alt="harry potter" />
+
+        <div className="quiz-card">
+          <img
+            src="Assets/css-quiz.jpg"
+            alt="Css Quiz"
+            className="card-image"
+          />
           <div className="image-text">
             <h3>CSS</h3>
             <p className="details">Test Your CSS Understanding here</p>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
